9383925a62fe0df0853c7f476a44f3f0410affe9
Mahender Lokini <115463358+Mahender-77@users.noreply.github.com>
2024-11-09 11:30:14 +0530

    Update App.jsx
    updated user page import

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,7 +7,7 @@ import {Routes,Route} from 'react-router-dom'
 import Home from './components/Home'
 import Signup from './components/Signup'
 import Login from './components/Login'
-import UserPage from './components/userPage'
+import UserPage from './components/UserPage'
 function App() {
  
 
